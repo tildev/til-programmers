53c45e87bbd7eb0fb8e60a7f8dcca5a25c93f021
tildev <tildevelove@gmail.com>
2023-03-08 18:05:16 +0900

    [Add] lesson 161990 다른 풀이 추가

--- src/lc30/l161990/Solution.java
+++ src/lc30/l161990/Solution.java
@@ -31,11 +31,29 @@ public class Solution {
         return answer;
     }
 
+    public int[] solution1(String[] wallpaper) {
+        int minX = Integer.MAX_VALUE;
+        int minY = Integer.MAX_VALUE;
+        int maxX = Integer.MIN_VALUE;
+        int maxY = Integer.MIN_VALUE;
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[i].length(); j++) {
+                if (wallpaper[i].charAt(j) == '#') {
+                    minX = Math.min(minX, i);
+                    minY = Math.min(minY, j);
+                    maxX = Math.max(maxX, i);
+                    maxY = Math.max(maxY, j);
+                }
+            }
+        }
+        return new int[]{minX, minY, maxX + 1, maxY + 1};
+    }
+
     public static void main(String[] args) {
         // String[] wallpaper = {".#...", "..#..", "...#."};
         // String[] wallpaper = {"..", "#."};
         String[] wallpaper = {"..........", ".....#....", "......##..", "...##.....", "....#....."};
-        int[] result = new Solution().solution(wallpaper);
+        int[] result = new Solution().solution1(wallpaper);
 
         for (int i : result) {
             System.out.println(i);
